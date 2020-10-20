020c5da27b84295d44a2a73fa1a6547c98c4a03a
Roberto Ledesma R <67805795+ElRobert17L@users.noreply.github.com>
2020-10-19 21:17:41 -0500

    Proyectos realizados con Struts2

---
+++ Leccion06/LoginStruts2/src/main/java/beans/actions/LoginAction.java
@@ -0,0 +1,14 @@
+package beans.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.*;
+
+@Result(name="success", location = "/WEB-INF/content/login.jsp")
+public class LoginAction extends ActionSupport{
+    
+    @Action("login")
+    public String execute() {
+        return SUCCESS;
+    }
+    
+}
